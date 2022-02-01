8940ea8cf5ca2ac44c280278fe993c4503a944df
unknown <marinecorbel@yahoo.fr>
2022-02-01 17:03:50 +0100

    chat room front v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
+
+let socket;
+const PORT = "http://localhost:4000";
 
 function App() {
+  const [logIn, setLogIn] = useState(false);
+  const [room, setRoom] = useState("my room");
+  const [username, setUsername] = useState("");
+
+  const [message, setMessage] = useState("");
+  const [messagesSent, setMessagesSent] = useState([]);
+  /*  console.log("ma list", messagesSent); */
+  const connectToRoom = () => {
+    socket.emit("join_room", room);
+    setLogIn(true);
+  };
+
+  const sendMessage = async () => {
+    let messageContent = {
+      room: room,
+      content: { author: username, message: message },
+    };
+    if (message === "") {
+      return;
+    }
+    socket.emit("send_message", messageContent);
+
+    /*  console.log("mon message", messageContent.content); */
+    setMessagesSent([...messagesSent, messageContent.content]);
+    setMessage("");
+    /*  console.log(messagesSent); */
+  };
+
+  const handleDisconnect = () => {
+    let messageContent = {
+      room: room,
+      username: username,
+    };
+    socket.emit("disconnected", messageContent);
+    setLogIn(false);
+  };
+
+  useEffect(() => {
+    socket = io(PORT, {
+      cors: {
+        origin: "http://localhost:4000",
+      },
+    });
+  }, [PORT]);
+
+  useEffect(() => {
+    socket.on("receive-message", (data) => {
+      setMessagesSent([...messagesSent, data]);
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!logIn ? (
+        <div className="logIn">
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          ></input>
+          <input
+            type="text"
+            placeholder="Room"
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          ></input>
+          <button onClick={connectToRoom}> Enter chat </button>
+        </div>
+      ) : (
+        <div className="container">
+          <div className="header">
+            <div className="disconnect-box">
+              <button onClick={handleDisconnect}>disconnect</button>
+            </div>
+          </div>
+          <div className="chat-container">
+            <div className="title">
+              <h1>welcome on : {room}</h1>
+            </div>
+
+            <div
+              className="chat-screen"
+              id={message.author === username ? "youbox" : "othersbox"}
+            >
+              {messagesSent.map((message, index) => {
+                return (
+                  <div
+                    id={message.author === username ? "you" : "others"}
+                    className="mess-box"
+                    key={index}
+                  >
+                    <span
+                      className={message.author === username ? "you" : "others"}
+                    >
+                      {message.author}:{message.message}
+                    </span>
+                  </div>
+                );
+              })}{" "}
+            </div>
+            <div className="chat-message">
+              <input
+                type="text"
+                value={message}
+                placeholder="message"
+                onChange={(event) => {
+                  setMessage(event.target.value);
+                }}
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                    sendMessage();
+                  }
+                }}
+              ></input>
+              <button onClick={sendMessage}>Send</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
